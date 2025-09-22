fce04fb3f2286394c41b3314ffb556dd81170ca3
Ridoy Baidya <ridoybaidya2@gmail.com>
2025-09-23 00:26:47 +0600

    Update Brand Logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/Logo1.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
